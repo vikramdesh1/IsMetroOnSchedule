14b54b762a0ed00fb182a9c53209b8047e0617dc
Vikram Deshmukh <vikramdesh1@gmail.com>
2016-02-18 15:05:58 -0500

    Create myscript.js

---
+++ myscript.js
@@ -0,0 +1,50 @@
+  $(document).ready(function() {
+    $("#header").fadeIn(1000, function(){
+      $.ajax({
+      url: "https://api.wmata.com/Incidents.svc/json/Incidents",
+      beforeSend: function(xhrObj) {
+        xhrObj.setRequestHeader("api_key", "affa4c8afe0f49aaaa0e588b26cfc4aa");
+      },
+      type: "GET"
+      })
+    .done(function(data) {
+      console.log(data.Incidents);
+      if (data.Incidents.length > 0) {
+        
+        for(i=0; i<data.Incidents.length; i++) {
+        var incident = data.Incidents[i];
+        var detail = "<br/> <div class=\"row\"> <div class=\"col-xs-4\"> </div> <div id=\"detail" + i +"\"" + " class=\"col-xs-4 text-left\">"
+        + "<br/><strong>Incident Type : </strong>" + incident.IncidentType
+        + "<br/><strong>Lines Affected : </strong>" + incident.LinesAffected
+        + "<br/><strong>Description : </strong>" + incident.Description
+        + "</div> <div class=\"col-xs-4\"> </div> </div>";
+        $('#details').append(detail);
+        }
+        var message = "No, there are currently " + data.Incidents.length + " issue(s) on Metro.";
+        updateStatusText(message);
+        
+        
+      } else {
+        updateStatusText("Yes, all trains appear to be running on time.");
+      }
+      })
+    .fail(function() {
+      console.log("Something went wrong...");
+      updateStatusText("A problem occured while checking for incidents. Please try again later.");
+      });
+    });
+});
+
+function updateStatusText(text) {
+    $('#message').fadeOut(function(){
+    $('#message p').text(text);
+    $('#message').fadeIn(function() {
+      showDetails();
+    });
+  });
+      
+};
+
+function showDetails() {
+  $('#details').slideDown();
+}
